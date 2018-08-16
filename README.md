880c8a582da7065aa78f85cd7a0d1fd898c16af4
cmaniscalchi <claudia.maniscalchi@gmail.com>
2018-08-16 13:16:47 -0400

    sidebar added

--- README.md
+++ README.md
@@ -1,1 +1,17 @@
 # js-evernote-frontend
+
+
+http://localhost:3000/api/v1/notes
+http://localhost:3000/api/v1/users
+http://localhost:8080/ ---> "python -m SimpleHTTPServer 8080"
+
+make a plan, break it down into steps, do them in order
+
+once you fetch all users and the promise is fulfilled, we can translate those into dom objects. the last callback in that promise chain should be to a function where the dom manipulation happens
+
+maybe, one function which gets at all of the ones, one which iterates through them, etc.
+
+
+Tasks:
+
+add event listener onto the sidebar buttons. on click, display the note title and body in the main content ares
--- index.js
+++ index.js
@@ -1,18 +1,43 @@
 document.addEventListener("DOMContentLoaded", () => {
 
-
-  console.log("DOM loaded");
-
 })
-const notesUrl = 'http://localhost:3000/api/v1/notes';
+
 const usersUrl = 'http://localhost:3000/api/v1/users';
-const post1 = {title:"Claudia's Test", body:'Posting my first note!', user:'claudia-new'}
-const config = {
-	method: "POST",
-	headers: {'Content-Type': 'application/json'},
-	body: JSON.stringify(post1)
+const notesUrl = 'http://localhost:3000/api/v1/notes';
+
+const note = {user_id: "1", title:"Claudia's Test", body:'Posting my first note!'}
+const noteConfig = {
+  method: "POST",
+  headers: {'Content-Type': 'application/json'},
+  body: JSON.stringify(note)
 }
 
-fetch(notesUrl).then(res => res.json()).then(response => console.log(response));
+// ---get notes---
+fetch(notesUrl).then(res => res.json()).then(data => displayNotesInSidebar(data));
 
-fetch(notesUrl, config).then(res => res.json()).then(response => console.log(response))
+// ---create new note---
+// fetch(notesUrl, noteConfig).then(res => res.json()).then(response => console.log(response))
+
+// ---get user---
+// fetch(usersUrl).then(res => res.json()).then(data => displayUsers(data));
+
+function displayNotesInSidebar(data) {
+  const sidebarNotes = document.getElementById('sidenav')
+  data.forEach(note => {
+    let div = document.createElement('div')
+    div.id = note.id
+    div.className = "note-titles"
+    div.innerHTML =
+    `<p>${note.title}</p>
+    <button>Read</button>
+    <hr>`
+    sidebarNotes.appendChild(div)
+  })
+}
+
+
+
+
+// document.addEventListener("click", (event => {
+//   if (event.target.innerText === "Read") {console.log(BUTTON!)}
+// }))
